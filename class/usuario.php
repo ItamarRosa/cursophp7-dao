5ff5cc72ef87f3c5c06b5661af1c4b10afe12b57
Itamar Rosa <itamar.santos@gmail.com>
2019-02-13 10:02:25 -0200

    inserir e alterar

--- class/usuario.php
+++ class/usuario.php
@@ -43,12 +43,7 @@ class Usuario {
 
 		if(count($results)> 0){
 
-			$row = $results[0];
-
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			$this->setData($results[0]);;
 
 		}
 	}
@@ -84,20 +79,65 @@ class Usuario {
 
 		if(count($results)> 0){
 
-			$row = $results[0];
-
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			$this->setData($results[0]);
 
 		} else {
 			throw new Exception("Login e/ou senha inválidos.");
 			
-
 		}
 
+	}
 
+
+	public function setData($data){
+
+			$this->setIdusuario($data['idusuario']);
+			$this->setDeslogin($data['deslogin']);
+			$this->setDessenha($data['dessenha']);
+			$this->setDtcadastro(new DateTime($data['dtcadastro']));
+
+	}
+
+
+	//metodo insert para criar uma usuario novo apartir da nossa classe de usuario (foi criado uma procedure para inserir novo usa)
+	public function insert(){
+		
+		$sql = new sql();
+		$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha()
+
+		));
+
+		if(count($results)>0){
+
+			$this->setData($results[0]);
+		}	
+
+	}
+
+
+//metodo criado abaixo para realizar update (alteração) dos usuarios. Os parametros são passados no própio método chamado update para serem preenchidos no detro da query. Outra coisa, como está sendo colocado denntro da query  a partir  do getDeslogin e o do getDessenha e então vamos definílas dentro do Objeto.
+public function update($login, $password){
+
+	$this->setDeslogin($login);
+	$this->setDessenha($password);	
+	
+	$sql = new sql();
+	
+	$sql->query("UPDATE tb_usuarios set deslogin = :LOGIN, desenha = :PASSWORD WHERE idusuario = :ID", array(
+		':LOGIN'=>$this->getDeslogin(),
+		'PASSWORD'=>$this->getDessenha(),
+		':ID'=>$this->getIdusuario()
+	));
+}
+
+
+
+//metodo criado para que seja passado os valores no momento em que o objeto for instanciado (para facilitar insert). Toda vez que chamar a classe usuario teríamos que passar o usuario e  a senha, para nã ose torna obrigatoriedade =e so colocar = "". Ou seja se voce chamar beza se não chamar ele ja vai alimentar eles com vazio dessa forma não dando erro.
+	public function __construct($login = "" ,$password = ""){ 
+		$this->setDeslogin($login);
+		$this->setDessenha($password);	
 	}
 
 
--- index.php
+++ index.php
@@ -12,14 +12,32 @@ echo $root;
 //$lista = usuario::getlist();
 //echo json_encode($lista);
 
-//Carrega uma lista de usuários pelo login
+//Carrega uma lista de usuários pelo login----------------------------
 //$search = usuario::search("jo");
 //echo json_encode($search);
 
-//Carregando usuário usando o login e a senha
+//Carregando usuário usando o login e a senha (FAZENDO LOGIN)------------------------
+//$usuario = new Usuario();
+//$usuario->login("root","$#$%&@");
+
+//echo $usuario;
+
+//Criando úm novo usuario------------------------------
+//$aluno = new Usuario("aluno", "@lun0"); //foi craido metodo mágico para passar os valores quando o objeto for instanciado
+
+//outra foram de forma passar o usuario e a senha.
+//$aluno->setDeslogin("aluno");
+//$aluno->setDessenha("@lun0");
+
+//$aluno->insert(); outra forma
+
+//echo $aluno;
+
 $usuario = new Usuario();
-$usuario->login("root","$#$%&@");
+
+$usuario->loadById(8);//informar o id a ser alterado
+
+$usuario->update("professor","#U$$&&%");
 
 echo $usuario;
-
 ?>
